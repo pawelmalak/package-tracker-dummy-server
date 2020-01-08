d57fcce25dd23cf640ee0be675551338ef53e257
unknown <pawel999@icloud.com>
2020-01-08 12:52:49 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const public = path.join(__dirname, '../public');
+app.use(express.static(public));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(public, 'index.html'));
+})
+
+app.get('/inpost', (req, res) => {
+  res.sendFile(path.join(public, 'inpost.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+})
